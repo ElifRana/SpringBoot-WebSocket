154021a1f532d253c2bb08c2ab867ee5699e3058
Elif Rana YILANCI <72026809+ElifRana@users.noreply.github.com>
2022-02-17 13:30:12 +0300

    WebSocket

--- src/main/java/com/example/springbootwebsocket/config/WsConfig.java
+++ src/main/java/com/example/springbootwebsocket/config/WsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -12,12 +13,23 @@ public class WsConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/elifrana").withSockJS();
+        registry.addEndpoint("/elifrana").withSockJS()
+                .setStreamBytesLimit(15 * 1024)
+                .setHttpMessageCacheSize(15 * 1024);
+        // registry.addEndpoint("/elifrana").setAllowedOrigins("*").withSockJS();
+        // kim gelirse gelsin bağlansın demek için setAllowedOrigins("*") kullanıyoruz
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(60 * 1000)
+                .setSendBufferSizeLimit(200 * 1024 * 1024)
+                .setMessageSizeLimit(200 * 1024 * 1024);
     }
 }
--- src/main/java/com/example/springbootwebsocket/controller/ChatController.java
+++ src/main/java/com/example/springbootwebsocket/controller/ChatController.java
@@ -1,17 +1,25 @@
 package com.example.springbootwebsocket.controller;
 
 import com.example.springbootwebsocket.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    @Autowired
+    //mesajı kendimiz özelleştirip gönderebiliyoruz.
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/chat.register")
-    @SendTo("/topic/public")
+    @SendTo("/topic/public")   //herkes için
+    // @SendToUser()         //tek bir kullanıcıya göndermek için
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
@@ -21,6 +29,14 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
+    }
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, ChatMessage chatMessage) {
+
+        System.out.println("handling send message: " + chatMessage + " to: " + to);
+        simpMessagingTemplate.convertAndSend("/topic/messages/" + to, chatMessage);
+
     }
 
 }
